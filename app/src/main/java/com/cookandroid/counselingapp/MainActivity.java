e3cac0a9f2912d5d04bc7be175065a90f8e7c606
gaeun <geun5744@gmail.com>
2022-11-22 01:29:26 +0900

    ADD firebase 연동

--- app/src/main/java/com/cookandroid/counselingapp/MainActivity.java
+++ app/src/main/java/com/cookandroid/counselingapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.cookandroid.counselingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -8,6 +9,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText chatText;
     private Button sendButton;
 
+    private DatabaseReference myRef;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //입력창에 메시지를 입력 후 버튼클릭했을 때
                 String msg = chatText.getText().toString();
 
                 if(msg != null){
@@ -41,12 +52,15 @@ public class MainActivity extends AppCompatActivity {
                     chat.setName(nickname);
                     chat.setMsg(msg);
 
+                    //메시지를 파이어베이스에 보냄.
+                    myRef.push().setValue(chat);
+
                     chatText.setText("");
                 }
 
             }
         });
-
+        //리사이클러뷰에 어댑터 적용
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(this);
@@ -55,5 +69,41 @@ public class MainActivity extends AppCompatActivity {
         chatList = new ArrayList<>();
         adapter = new ChatAdapter(chatList, nickname);
         recyclerView.setAdapter(adapter);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("message");
+
+        //데이터들을 추가, 변경, 제거, 이동, 취소
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                //어댑터에 DTO추가
+                Chat chat = snapshot.getValue(Chat.class);
+                ((ChatAdapter)adapter).addChat(chat);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
+
+
 }
