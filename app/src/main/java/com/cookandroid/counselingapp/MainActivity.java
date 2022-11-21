3d65b956d6bc8bfd0c48b71625c620f4ad4246f7
gaeun <geun5744@gmail.com>
2022-11-22 00:08:36 +0900

    UPDATE UI와 Recyclerview 어댑터 연결

--- app/src/main/java/com/cookandroid/counselingapp/MainActivity.java
+++ app/src/main/java/com/cookandroid/counselingapp/MainActivity.java
@@ -1,14 +1,59 @@
 package com.cookandroid.counselingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private List<Chat> chatList;
+    private String nickname = "익명1";
+
+    private EditText chatText;
+    private Button sendButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        chatText = findViewById(R.id.chatText);
+        sendButton = findViewById(R.id.sendButton);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = chatText.getText().toString();
+
+                if(msg != null){
+                    Chat chat = new Chat();
+                    chat.setName(nickname);
+                    chat.setMsg(msg);
+
+                    chatText.setText("");
+                }
+
+            }
+        });
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        chatList = new ArrayList<>();
+        adapter = new ChatAdapter(chatList, nickname);
+        recyclerView.setAdapter(adapter);
     }
 }
