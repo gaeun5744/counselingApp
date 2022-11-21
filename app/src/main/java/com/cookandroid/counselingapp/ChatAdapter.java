b07e83306f3acd0c9300ac1daf105ab17614f38d
gaeun <geun5744@gmail.com>
2022-11-22 01:43:37 +0900

    UPDATE Recyclerview 사용자 구분 정렬

--- app/src/main/java/com/cookandroid/counselingapp/ChatAdapter.java
+++ app/src/main/java/com/cookandroid/counselingapp/ChatAdapter.java
@@ -23,13 +23,13 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.MyViewHolder> 
     private String name;
 
 
-    public ChatAdapter(List<Chat> chatData, String name) {
+    public ChatAdapter(List<Chat> chatData, String name){
         //MainActivity.java에서 받은 데이터들을 저장
         chatList = chatData;
         this.name = name;
     }
 
-    public static class MyViewHolder extends RecyclerView.ViewHolder {
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
         public TextView nameText;
         public TextView msgText;
         public LinearLayout msgLinear;
@@ -56,7 +56,7 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.MyViewHolder> 
     public ChatAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
         //inflation 과정
-        LinearLayout linearLayout = (LinearLayout) LayoutInflater.from(parent.getContext()).inflate(R.layout.msg_item, parent, false);
+        LinearLayout linearLayout = (LinearLayout) LayoutInflater.from(parent.getContext()).inflate(R.layout.msg_item,parent,false);
 
         MyViewHolder myViewHolder = new MyViewHolder(linearLayout);
 
@@ -73,7 +73,7 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.MyViewHolder> 
         holder.nameText.setText(chat.getName());
         holder.msgText.setText(chat.getMsg());
 
-        if (chat.getName().equals(this.name)) {
+        if(chat.getName().equals(this.name)){
             //사용자가 저장된 이름과 같을 시 오른쪽으로 정렬
             holder.nameText.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_END);
             holder.msgText.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_END);
@@ -93,12 +93,12 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.MyViewHolder> 
     //메시지아이템 갯수세기
     @Override
     public int getItemCount() {
-        return chatList == null ? 0 : chatList.size();
+        return chatList == null ? 0: chatList.size();
     }
 
     //메시지아이템의 추가 및 적용
-    public void addChat(Chat chat) {
+    public void addChat(Chat chat){
         chatList.add(chat);
-        notifyItemInserted(chatList.size() - 1);
+        notifyItemInserted(chatList.size()-1);
     }
 }
